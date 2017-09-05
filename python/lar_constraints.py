d98e2498aae249dab28c6f5aba23ace7d52e1144
David Roell <breeroell@gmail.com>
2017-09-05 15:00:20 -0600

    closes #100, v681 constraint

--- python/lar_constraints.py
+++ python/lar_constraints.py
@@ -10,7 +10,7 @@ class lar_constraints(object):
 		"v640_const", "v641_const", "v643_const", "v644_const", "v645_const", "v647_const", "v648_const", "v649_const", "v650_const", "v651_const", "v652_const",
 		"v654_const", "v655_const", "v656_const", "v657_const", "v658_const", "v661_const", "v662_const", "v663_const", "v664_const", "v666_const", "v667_const",
 		"v668_const", "v669_const", "v670_const", "v671_const", "v672_const", "v673_const", "v674_const", "v675_const", "v676_const", "v677_const", "v678_const",
-		"v679_const", "v680_const"
+		"v679_const", "v680_const", "v681_const"
 		]
 		self.tracts = tracts
 		self.counties = counties
@@ -794,8 +794,12 @@ class lar_constraints(object):
 			row["dti"] = "NA"
 		return row
 
-	#V681: 1) Combined Loan-to-Value Ratio must be either a number greater than 0 or NA, and cannot be left blank.
-	#      2) If Action Taken equals 4, 5, or 6, then Combined Loan-to-Value ratio must be NA. 
+	def v681_const(self, row): 
+		"""1) Combined Loan-to-Value Ratio must be either a number greater than 0 or NA, and cannot be left blank.
+		2) If Action Taken equals 4, 5, or 6, then Combined Loan-to-Value ratio must be NA."""
+		if row["action_taken"] in ("4", "5", "6"):
+			row["cltv"] = "NA"
+		return row
 
 	#V682: 1) Loan Term must be either a whole number greater than zero or NA, and cannot be left blank. 
 	#      2) If Reverse Mortgage equals 1, then Loan Term must be NA.
