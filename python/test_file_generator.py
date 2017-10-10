9f51efd041d09b455179612be82ed73ca27dc906
David Roell <breeroell@gmail.com>
2017-10-10 10:29:57 -0600

    continues #16

--- python/test_file_generator.py
+++ python/test_file_generator.py
@@ -27,11 +27,32 @@ class test_data(object):
 			self.ts_df = pd.DataFrame(data=ts_data, dtype=object, columns=self.ts_field_names)
 			self.lar_df  = pd.DataFrame(data=lar_data, dtype=object, columns=self.lar_field_names)
 
-	def read_data_frames(self):
+	def read_data_frames(self, ts_df, lar_df):
 		"""Receives two pandas dataframes (one TS and one LAR) and stores them as class objects."""
-	def write_file(self, edit_name, path):
-		"""Writes a dataframe object to a pipe delimited text file in the specified location."""
-		#
+		self.ts_df = ts_df
+		self.lar_df = lar_df
+
+	def write_file(self, edit_name, path, ts_df, lar_df):
+		 """Takes a TS row as a dictionary and LAR data as a dataframe. Writes LAR data to file and
+    		re-reads it to combine with TS data to make a full file."""
+		#make directories for files if they do not exist
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+		#write LAR dataframe to file
+		parts_dir = directory+"file_parts/"
+		if not os.path.exists(parts_dir):
+			os.makedirs(parts_dir)
+
+		lar_input.to_csv(parts_dir + "lar_data.txt", sep="|", header=False, index=False, index_label=False)
+		#load LAR data as file rows
+		with open(parts_dir + "lar_data.txt", 'r') as lar_data:
+			lar = lar_data.readlines()
+
+		with open(directory + name, 'w') as final_file:
+			final_file.write("|".join(ts_input.values())+"\n")
+		for line in lar:
+		    final_file.write("{line}".format(line=line))
 	#edits will be broken out into sub parts as in the rules_engine.py class. This will allow test files to be generated such that they fail conditions inside each edit.
 	#When possible each file will only fail the condition listed in the file name. There will be cases when test files fail additional edits, these cases will be documented
 	#to the extent possible.
