58b6ba0eb5d36a0b008b960dd644e0fcbe48e797
David Roell <breeroell@gmail.com>
2017-10-19 08:57:00 -0600

    closes #219

--- python/test_file_generator.py
+++ python/test_file_generator.py
@@ -1439,3 +1439,14 @@ class test_data(object):
 		lar.app_score_name = random.choice(("10", ""))
 		print("writing {name}".format(name=name))
 		utils.write_file(name=name, path=path, ts_input=ts, lar_input=lar)
+
+	def v661_file(self):
+		"""Set app credit score to 8888. Set app score model to random of 1-8."""
+		name = "v661.txt"
+		path = self.validity_path
+		ts = self.ts_df.copy()
+		lar = self.lar_df.copy()
+		lar.app_credit_score = "8888"
+		lar.app_score_name = random.choice(("1", "2", "3", "4", "5", "6", "7", "8"))
+		print("writing {name}".format(name=name))
+		utils.write_file(name=name, path=path, ts_input=ts, lar_input=lar)
