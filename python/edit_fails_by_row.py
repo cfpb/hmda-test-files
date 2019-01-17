4a5c019839ac802c221dc80b2ad8b80b2c2302b3
Michael Furey <Michael.Furey@cfpb.gov>
2019-01-17 08:15:50 -0800

    add more doc strings and adjust name of the file

--- python/edit_fails_by_row.py
+++ python/edit_fails_by_row.py
@@ -14,19 +14,19 @@ from large_test_files import LargeTestFiles
 
 from large_test_files import unique_uli
 
-class percentQualityFails(object):
+class EditFailsByRow(object):
 
 	def __init__(self, passes_all_filepath, passes_all_filename, 
 		fails_all_filepath, fails_all_filename):
 		"""The class instantiates with a filepath and name to a file that 
 		passes every edit, and a filepath and name to a file that fails 
-		a quality edit or multiple quality edits for every row."""
+		a an edit or multiple edits for every row."""
 		
 		#Loading TS and LAR data for the clean file. 
 		self.ts_data, self.passes_all_lar = utils.read_data_file(path=passes_all_filepath, 
                                                  data_file=passes_all_filename)
 
-		#Loading TS and LAR data for the file that fails quality edits for every row. 
+		#Loading TS and LAR data for the file that fails a set of edits for every row. 
 		self.ts_data, self.fails_all_lar = utils.read_data_file(path=fails_all_filepath,
 												 data_file=fails_all_filename)
 
@@ -34,34 +34,40 @@ class percentQualityFails(object):
 		self.passes_all_lar = self.passes_all_lar.iloc[0:1]
 		print(self.passes_all_lar)
 
+		#Storing the LEI in a global variable. 
 		self.lei = self.ts_data.iloc[0][14]
 
-
-		#Storing the first LAR row of the file that fails quality edits for every row. 	
+		#Storing the first LAR row of the file that fails a set of edits for every row. 	
 		self.fails_all_lar = self.fails_all_lar.iloc[0:1]
 		print(self.fails_all_lar)
 
 		#Prints a statement indicating that the object has been instantiated. 
 		print("Instantiated")
 
-	def create_quality_fails_by_row(self, rows_failed=50, rows_total=100, 
-		output_filepath="../edits_files/percent_quality_fails/"):
+	def create_edit_fails_by_row_file(self, rows_failed=50, rows_total=100, 
+		output_filepath="../edits_files/edit_fails_by_row/"):
 
-		"""Creates a file that fails quality edits at a 
+		"""Creates a file that fails a set of edits by a 
 		certain number of rows."""
 
-		#Creates 
+		#Creates LAR rows that pass every edit.  
 		new_passes = pd.concat([self.passes_all_lar]*(rows_total-rows_failed))
 
+		#Creates a specified number of LAR rows that fail a set of edits.
 		new_fails = pd.concat([self.fails_all_lar]*(rows_failed))
 
+		#Concatenates the LAR rows above to form the specified number of rows in total. 
 		new_lar = pd.concat([new_passes, new_fails])
 
+		#Creates a new set of unique ulis to avoid creating duplicate rows.  
 		new_lar = unique_uli(new_lar_df = new_lar, lei=self.lei)
 
+		#Writes the file to the output filepath specified in the class instantiation. 
 		utils.write_file(ts_input=self.ts_data, lar_input=new_lar,
 			path=output_filepath, name=str(rows_failed)+"_rows_failed_"+str(rows_total)+"_total_rows.txt")
 
+		#Prints a statement indicating that the file has been creating in the specified
+		#filepath. 
 		statement = (str("{:,}".format(rows_failed)) + 
             " rows failed " + str("{:,}".format(rows_total)) + 
             " rows total stored in " + output_filepath)
