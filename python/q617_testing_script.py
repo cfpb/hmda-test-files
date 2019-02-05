bcfe21c1487874f9ba91b58efc61fbfac56e08ea
Michael Furey <Michael.Furey@cfpb.gov>
2019-02-05 15:53:26 -0800

    make yaml file for row by row file modifications

--- python/q617_testing_script.py
+++ python/q617_testing_script.py
@@ -1,108 +1,14 @@
-#The following code imports the required packages.
-import math
-import json
-import os
-import pandas as pd
-import random
-import string
-import time
-import yaml
-import shutil as sh
+#This script creates a test file for Q617, using the row_by_row_modification
+#function in utils. The row_by_row_modification_yaml file is used with the function
+#to make modifications to a file specifying rows, columns, and values.
+
+#The resulting file is placed in the edits_files directory under
+# "../edits_files/Q617_testing/".
+
+#The following code imports the utils package.
 import utils
 
-from large_test_files import unique_uli
-
-"""
-This script creates 5 files for testing the Q617 edit with the same
-loan amount, property value, and CLTV. Each file has CLTV rounded 
-to a different value. Each file has loan amounts and property values 
-rounded to either 0, 1, or two decimal places. 
-
-CLTV is calculated by dividing loan amount by the property value for each row
-and rounding to the place specified in the filename. 
-
-"""
-
-def passes_cltv(cltv_rounded = 0, row=None):
-    """Creates CLTV values from loan amount
-    and property value"""
-    
-    #Divides loan amount by provery value.
-    ltv = row['loan_amount']/row['property_value']
-    
-    #Calculates CLTV by rounding LTV to a place specified in the function.
-    cltv = round(ltv, cltv_rounded)
-    return cltv
-
-
-def cltv_passes_all_setup(lar_data=None, ts_data=None, row_count=None):
-    """Creates 5 files that should pass Q617"""
-
-    #Stores random floats for loan_amount and property_value. 
-    loan_amount = random.uniform(100000, 1000000)
-    property_value = random.uniform(100000, 1000000)
-
-    #Creates empty lists for storing the same values rounded to different places. 
-    loan_amount_lst = []
-    property_value_lst = []
-
-    #Stores loan amount and property value in the list above, rounded to either
-    #0, 1, or 2 decimal places. 
-    
-    for i in [0, 1, 2]:
-        loan_amount_lst.append(round(loan_amount, i))
-    #print(loan_amount_lst)
-
-    for i in [0, 1, 2]:
-        property_value_lst.append(round(property_value, i))
-    #print(property_value_lst)
-
-
-    #Iterates over 0-4 decimal places for CLTV values. 
-    for i in [0, 1, 2, 3, 4]:
-        #Iterates over each dataframe row to place a property value, 
-        #a loan amount and a calculated CLTV. 
-        for index, row in lar_data.iterrows():
-            row['property_value'] = random.choice(property_value_lst)
-            row['loan_amount'] = random.choice(loan_amount_lst)
-            row['cltv'] = passes_cltv(cltv_rounded = i, row=row)
-
-        #Stores the LAR dataframe as a new LAR dataframe. 
-        new_lar_df = lar_data
-
-        #Stores the number of rows currently in the dataframe.
-        current_row = len(new_lar_df.index) 
-        
-        """Calculates a multiplier taking the ceiling function of 
-        the desired row count over the current row count.""" 
-        multiplier = math.ceil(row_count/current_row)
-        
-        """Concatenates data to produce the number of rows 
-        by the multiplier in a new LAR dataframe."""  
-        new_lar_df = pd.concat([new_lar_df]*int(multiplier))
-        
-        #Drops the number of rows to the count specified. 
-        if (row_count % current_row) != 0:
-            drop_rows = current_row - (row_count % current_row)
-            new_lar_df = new_lar_df[: - (drop_rows)]
-        
-        """Applies the unique_uli function from large_test_files 
-        to the new LAR dataframe to generate a unique set of ULIs."""  
-        new_lar_df = unique_uli(new_lar_df, ts_data['lei'])
-
-        #Modifies TS data for the new number of LAR entries.
-        ts_data["lar_entries"] = len(new_lar_df)
-
-        #Writes a Q617 file with CLTV rounded to i places. 
-        
-        utils.write_file(path="../edits_files/q617_testing/", ts_input=ts_data, 
-            lar_input=new_lar_df, name="test_file_cltv_"+str(i)+"_decimals.txt")
-
-        #Prints a statement indicating that a file has been created with a certain
-        #number of rows and CLTV rounded to i decimal places. 
-        print("Row count is " + str(len(new_lar_df)) + " for cltv rounded to "+str(i)+" decimal places.")
-
-#Stores a filepath for a clean test file, passing syntax and validity. 
+#Stores a filepath for a clean test file. 
 filepath = "../../hmda-platform/data/clean_test_files/bank_0/"
 
 #Stores a filename for the clean test file. 
@@ -119,6 +25,23 @@ lar_df = lar_df[(lar_df['loan_amount'] != 'Exempt') &
                 (lar_df['cltv'] != 'Exempt') &
                 (lar_df['cltv'] != 'NA')]
 
-#Initiates the function for creating Q617 test files. 
-new_lar_df = cltv_passes_all_setup(lar_data = lar_df, ts_data=ts_df, row_count=100)
+#Creates a five line file for each line in the row by row modification yaml.
+lar_df, ts_df = new_lar_rows(row_count=5, lar_df=lar_df, ts_df=ts_df)
 
+#Modifies the file based on the row by row modification yaml.
+lar_df = utils.row_by_row_modification(lar_df)
+
+#Stores the file path for the resulting file. 
+output_filepath = "../edits_files/Q617_testing/"
+
+#Stores the file name for the resulting file. 
+output_filename = "Q617_test.txt"
+
+#Writes a file to the filename and path. 
+utils.write_file(path=output_filepath, ts_input=ts_df, 
+    lar_input=lar_df, name=output_filename)
+
+#Outputs print statements indicating the number of LAR, the filename, 
+#and the filepath.
+print("Row count is " + str(len(lar_df)) + " for " + str(output_filename))
+print(str(output_filename) + " is stored in " + str(output_filepath))
--- python/utils.py
+++ python/utils.py
@@ -1,12 +1,28 @@
-#This file contains helper functions used by one or more classes in order to generate clean and failing synthetic data files.
+#This file contains helper functions used by one or more classes 
+#in order to generate clean and failing synthetic data files.
+
+#Imports the necessary libraries.
+import math
 import json
 import os
 import pandas as pd
+import yaml
+import utils
 
+#Imports lar_gen class to support the unique_uli function. 
+from lar_generator import lar_gen 
 
-state_codes = {'WA':'53', 'WI':'55', 'WV':'54', 'FL':'12', 'WY':'56', 'NH':'33', 'NJ':'34', 'NM':'33', 'NC':'37', 'ND':'38', 'NE':'31', 'NY':'36', 'RI':'44', 'NV':'32', 'CO':'08', 'CA':'06', 'GA':'13', 'CT':'09', 'OK':'40', 'OH':'39',
-					'KS':'20', 'SC':'45', 'KY':'21', 'OR':'41', 'SD':'46', 'DE':'10', 'HI':'15', 'PR':'43', 'TX':'48', 'LA':'22', 'TN':'47', 'PA':'42', 'VA':'51', 'VI':'78', 'AK':'02', 'AL':'01', 'AR':'05', 'VT':'50', 'IL':'17', 'IN':'18',
-					'IA':'19', 'AZ':'04', 'ID':'16', 'ME':'23', 'MD':'24', 'MA':'25', 'UT':'49', 'MO':'29', 'MN':'27', 'MI':'26', 'MT':'30', 'MS':'29', 'DC':'11'}
+#Instantiates lar_gen class as lar_gen. 
+lar_gen = lar_gen() 
+
+state_codes = {'WA':'53', 'WI':'55', 'WV':'54', 'FL':'12', 'WY':'56', 
+'NH':'33', 'NJ':'34', 'NM':'33', 'NC':'37', 'ND':'38', 'NE':'31', 'NY':'36', 
+'RI':'44', 'NV':'32', 'CO':'08', 'CA':'06', 'GA':'13', 'CT':'09', 'OK':'40', 
+'OH':'39', 'KS':'20', 'SC':'45', 'KY':'21', 'OR':'41', 'SD':'46', 'DE':'10', 
+'HI':'15', 'PR':'43', 'TX':'48', 'LA':'22', 'TN':'47', 'PA':'42', 'VA':'51', 
+'VI':'78', 'AK':'02', 'AL':'01', 'AR':'05', 'VT':'50', 'IL':'17', 'IN':'18',
+'IA':'19', 'AZ':'04', 'ID':'16', 'ME':'23', 'MD':'24', 'MA':'25', 'UT':'49', 
+'MO':'29', 'MN':'27', 'MI':'26', 'MT':'30', 'MS':'29', 'DC':'11'}
 
 def write_file(path=None, ts_input=None, lar_input=None, name="test_file.txt"):
 	"""Takes a TS row and LAR data as dataframes. Writes LAR data to file and
@@ -15,13 +31,16 @@ def write_file(path=None, ts_input=None, lar_input=None, name="test_file.txt"):
 	if not os.path.exists(path):
 		os.makedirs(path)
 	#check existence of file parts directory
-	parts_dir ="../edits_files/file_parts/"
+	parts_dir = "../edits_files/file_parts/"
 	if not os.path.exists(parts_dir):
 		os.makedirs(parts_dir)
+
 	#write TS dataframe to file
-	ts_input.to_csv(parts_dir + "ts_data.txt", sep="|", header=False, index=False, index_label=False)
+	ts_input.to_csv(parts_dir + "ts_data.txt", sep="|", header=False, 
+		index=False, index_label=False)
 	#write LAR dataframe to file
-	lar_input.to_csv(parts_dir + "lar_data.txt", sep="|", header=False, index=False, index_label=False)
+	lar_input.to_csv(parts_dir + "lar_data.txt", sep="|", header=False, 
+		index=False, index_label=False)
 	#load LAR data as file rows
 	with open(parts_dir + "lar_data.txt", 'r') as lar_data:
 		lar = lar_data.readlines()
@@ -33,7 +52,8 @@ def write_file(path=None, ts_input=None, lar_input=None, name="test_file.txt"):
 			final_file.write("{line}".format(line=line))
 
 def read_data_frames(ts_df=None, lar_df=None):
-	"""Receives two pandas dataframes (one TS and one LAR) and stores them as class objects."""
+	"""Receives two pandas dataframes (one TS and one LAR) and stores 
+	them as class objects."""
 	if ts_df is not None:
 		self.ts_df = ts_df
 	if lar_df is not None:
@@ -42,7 +62,8 @@ def read_data_frames(ts_df=None, lar_df=None):
 		raise ValueError("No data passed.\nNo data written to object.")
 
 def read_data_file(path="", data_file=None):
-	"""Reads a complete file (includes LAR and TS rows) into a pandas dataframe and returns them."""
+	"""Reads a complete file (includes LAR and TS rows) into a pandas 
+	dataframe and returns them."""
 	ts_schema = pd.DataFrame(json.load(open("../schemas/ts_schema.json", "r")))
 	lar_schema = pd.DataFrame(json.load(open("../schemas/lar_schema.json", "r")))
 	if data_file is not None:
@@ -55,8 +76,95 @@ def read_data_file(path="", data_file=None):
 			lar_rows = infile.readlines()
 			lar_data = [line.strip("\n").split("|") for line in lar_rows]
 			#create dataframes of TS and LAR data
-			ts_df = pd.DataFrame(data=ts_data, dtype=object, columns=ts_schema.field)
-			lar_df  = pd.DataFrame(data=lar_data, dtype=object, columns=lar_schema.field)
+			ts_df = pd.DataFrame(data=ts_data, dtype=object, 
+				columns=ts_schema.field)
+			lar_df  = pd.DataFrame(data=lar_data, dtype=object, 
+				columns=lar_schema.field)
 			return (ts_df, lar_df)
 	else:
 		raise ValueError("A data file mus be passed. No data have been written to the object")
+
+def unique_uli(new_lar_df=None, lei=None):
+    """
+    Generates a set of unique ULI's for a LAR dataframe.
+    """
+
+    #Copying over ULIs with the LEI.
+    new_lar_df["uli"] = new_lar_df["uli"].apply(lambda x: 
+        lei)  
+    
+    #Generates a loan ID as a random 23-character 
+    #string for each LEI.
+    new_lar_df["uli"] = new_lar_df["uli"].apply(lambda x: x + 
+        lar_gen.char_string_gen(23)) 
+    
+    #Adds a check digit to each.
+    new_lar_df['uli'] = new_lar_df["uli"].apply(lambda x: x + 
+        lar_gen.check_digit_gen(ULI=x))
+
+    #If ULIs are duplicated, the unique_uli function 
+    #is applied again.  
+    if len(new_lar_df['uli']) > len(set(new_lar_df['uli'])):
+        print("Re-Running")
+        self.unique_uli(new_lar_df)
+    else:
+        print("Unique ULIs Assigned")
+
+    return new_lar_df
+
+def new_lar_rows(row_count=None, lar_df=None, ts_df=None):
+    """
+    Returns LAR with a specified number of rows. 
+    Returns TS data with updated lar entries.
+    """
+
+    #Stores the LAR dataframe as a new LAR dataframe. 
+    new_lar_df = lar_df
+
+    #Stores the number of rows currently in the dataframe to calculate the 
+    #new number of rows to concatenate. 
+    current_row = len(new_lar_df.index) 
+    
+    #Calculates a multiplier taking the ceiling function of 
+    #the desired row count over the current row count.
+    multiplier = math.ceil(row_count/current_row)
+    
+    #Concatenates data to produce the number of rows 
+    #by the multiplier in a new LAR dataframe. 
+    new_lar_df = pd.concat([new_lar_df]*int(multiplier))
+    
+    #Drops the number of rows to the count specified. 
+    if (row_count % current_row) != 0:
+        drop_rows = current_row - (row_count % current_row)
+        new_lar_df = new_lar_df[: - (drop_rows)]
+    
+    #Applies the unique_uli function to the new LAR dataframe 
+    #to generate a unique set of ULIs. 
+    new_lar_df = utils.unique_uli(new_lar_df=new_lar_df, 
+    	lei=ts_df.iloc[0][14])
+
+    #Modifies TS data for the new number of LAR entries.
+    ts_df["lar_entries"] = len(new_lar_df)
+
+    return (new_lar_df, ts_df)
+
+def row_by_row_modification(lar_df=None):
+	"""Uses the inputs from the row_by_row 
+	modification yaml to modify a dataframe"""
+
+	#Opens the yaml_file. 
+	yaml_file ='row_by_row_modification.yaml'
+	with open(yaml_file, 'r') as f:
+		row_by_row = yaml.load(f)
+	
+	#Runs through each case in the file. 
+	#Modifies values for each column in a row specified in the yaml file. 
+	for case in row_by_row:
+		for column in row_by_row[case]["columns"]:
+			for key in column:
+				lar_df.iloc[row_by_row[case]["row"]][key] = column[key]
+
+	return lar_df
+					
+		
+
