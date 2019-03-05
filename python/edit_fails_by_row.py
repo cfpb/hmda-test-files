e6754bfc9b6c4744894a66b7f487fffa9cb00ab5
Michael Furey <Michael.Furey@cfpb.gov>
2019-03-05 14:09:35 -0800

    adjusting edit fails by row from class to script

--- python/edit_fails_by_row.py
+++ python/edit_fails_by_row.py
@@ -1,93 +1,75 @@
-import math
-import json
-import os
+#This script creates a file that takes rows from a file designed to fail 
+#all rows with the same set of edits and combines rows from a file that passes all edits.
+
 import pandas as pd
-import random
 import string
-import time
 import yaml
 import utils 
 
-class EditFailsByRow(object):
+#Loads custom_file_specifications yaml. 
+with open('configurations/custom_file_specifications.yaml') as f:
+	# Use safe_load instead of the load function. 
+	data_map = yaml.safe_load(f)
 
-	def __init__(self, passes_all_filepath, passes_all_filename, 
-		fails_all_filepath, fails_all_filename):
-		"""The class instantiates with a filepath and name to a file that 
-		passes every edit, and a filepath and name to a file that fails 
-		a an edit or multiple edits for every row."""
+#Loads and stores requisite variables. 
+bank_name = data_map['edit_fails_by_row']['bank_name']
+lei = data_map['edit_fails_by_row']['lei']
+tax_id = data_map['edit_fails_by_row']['tax_id']
+rows_failed = data_map['edit_fails_by_row']['rows_failed']
+rows_total = data_map['edit_fails_by_row']['rows_total']
+passes_all_filepath = data_map['edit_fails_by_row']['passes_all_filepath']
+passes_all_filename = data_map['edit_fails_by_row']['passes_all_filename']
+fails_all_filepath = data_map['edit_fails_by_row']['fails_all_filepath']
+fails_all_filename = data_map['edit_fails_by_row']['fails_all_filename']
+output_filepath = data_map['edit_fails_by_row']['output_filepath']
+output_filename = data_map['edit_fails_by_row']['output_filename']
 
-		#To create a file that has a certain number of clean rows and failed rows,
-		# two sets of TS and LAR data are loaded to create a file that
-		# has some rows that fail edits, and some rows that do not. 
-		# The constructor provides a way for users to input their files that pass cleanly
-		# and files that fail an edit or multiple edits for every row.  
-		
-		#Loading TS and LAR data for the clean file. 
-		self.clean_ts, self.clean_lar = utils.read_data_file(path=passes_all_filepath, 
-                                                 data_file=passes_all_filename)
+	
+#Loading TS and LAR data for the clean file. 
+passes_ts, passes_lar = utils.read_data_file(path=passes_all_filepath, 
+                                         data_file=passes_all_filename)
 
-		#Loading TS and LAR data for the file that fails a set of edits for every row. 
-		self.fail_ts, self.fail_lar = utils.read_data_file(path=fails_all_filepath,
-												 data_file=fails_all_filename)
+#Loading TS and LAR data for the file that fails a set of edits for every row. 
+fail_ts, fail_lar = utils.read_data_file(path=fails_all_filepath,
+										 data_file=fails_all_filename)
 
-		#Storing the first LAR row of the clean file. 
-		self.clean_lar = self.clean_lar.iloc[0:1]
-		#print(self.clean_lar)
+#Storing the first LAR row of the clean file. 
+passes_lar_row = passes_lar.iloc[0:1]
 
-		#Storing the LEI in a global variable. 
-		self.lei = self.clean_ts.iloc[0][14]
+#Storing the first LAR row of the file that fails a set of edits for every row. 	
+fails_lar_row = fail_lar.iloc[0:1]
 
-		#Storing the bank name as a global variable. 
-		self.bank_name = self.clean_ts.iloc[0][1]
+#Checks if the number of rows failed is less than the the number of rows in total.
+if rows_failed >= rows_total:
+	print("Sorry - rows_failed must be a number less than rows_total.")
 
-		#Setting LEI's for each by self.lei.
-		self.clean_lar["lei"] = self.lei
+else: 
 
-		self.fail_lar["lei"] = self.lei
+	#Creates LAR rows that pass every edit.  
+	rows_passed = pd.concat([passes_lar_row]*(rows_total-rows_failed))
 
-		#Storing the first LAR row of the file that fails a set of edits for every row. 	
-		self.fail_lar = self.fail_lar.iloc[0:1]
-		#print(self.fail_lar)
+	#Creates a specified number of LAR rows that fail a set of edits.
+	rows_failed = pd.concat([fails_lar_row]*(rows_failed))
 
-		#Prints a statement indicating that the object has been instantiated. 
-		print("Instantiated EditFailsbyRow Class")
+	#Concatenates the LAR rows above to form the specified number of rows in total. 
+	new_lar = pd.concat([rows_passed, rows_failed])
 
-	def create_edit_fails_by_row_file(self, rows_failed=50, rows_total=100, 
-		output_filepath= "../edits_files/edit_fails_by_row/"):
-		"""Creates a file that fails a set of edits by a 
-		certain number of rows."""
+	#Creates a new set of unique ULIs to avoid creating duplicate rows.  
+	new_lar = utils.unique_uli(new_lar_df = new_lar, lei=lei)
 
-		if rows_failed >= rows_total:
-			print("Sorry - rows_failed must be a number less than rows_total.")
+	#Replaces the TS number of rows with the rows in total.
+	passes_ts['lar_entries'] = len(new_lar.index)
 
-		else: 
-			#Naming an output filepath. 
-			output_filepath = output_filepath + str(self.bank_name) + "/"
+	#Changes the file to the institution features specified in the configuration file. 
+	new_ts, new_lar = utils.change_bank(ts_data=passes_ts, lar_data=new_lar, 
+		new_bank_name=bank_name, new_lei=lei, new_tax_id=tax_id)
 
-			#Creates LAR rows that pass every edit.  
-			new_rows_passes = pd.concat([self.clean_lar]*(rows_total-rows_failed))
+	#Writes the file to the output filepath specified in the configuration file. 
+	utils.write_file(ts_input=new_ts, lar_input=new_lar,
+		path=output_filepath, name=output_filename)
 
-			#Creates a specified number of LAR rows that fail a set of edits.
-			new_rows_fails = pd.concat([self.fail_lar]*(rows_failed))
+	#Prints a statement indicating that the file has been creating in the specified
+	#filepath. 
 
-			#Concatenates the LAR rows above to form the specified number of rows in total. 
-			new_lar = pd.concat([new_rows_passes, new_rows_fails])
-
-			#Creates a new set of unique ulis to avoid creating duplicate rows.  
-			new_lar = utils.unique_uli(new_lar_df = new_lar, lei=self.lei)
-
-			#Replaces the TS number of rows with the rows in total.
-			self.clean_ts['lar_entries'] = str(rows_total)
-
-			#Writes the file to the output filepath specified in the class instantiation. 
-			utils.write_file(ts_input=self.clean_ts, lar_input=new_lar,
-				path=output_filepath, name=str(rows_failed)+"_rows_failed_"+str(rows_total)+"_total_rows.txt")
-
-			#Prints a statement indicating that the file has been creating in the specified
-			#filepath. 
-			statement = (str("{:,}".format(rows_failed)) + 
-	            " rows failed " + str("{:,}".format(rows_total)) + 
-	            " rows total stored in " + output_filepath)
-
-			print(statement)
+	print("New File Created in {output_filepath}".format(output_filepath=output_filepath))
 		
