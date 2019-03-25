78867a5ccbf48396360df64039882e54b8d1c13d
Michael Furey <Michael.Furey@cfpb.gov>
2019-03-25 09:44:50 -0700

    adding new crosswalk data and generalizing variable names

--- python/rules_engine.py
+++ python/rules_engine.py
@@ -13,7 +13,7 @@ import utils
 
 class rules_engine(object):
 	"""docstring for ClassName"""
-	def __init__(self, lar_schema=None, ts_schema=None, year=2018, cbsa_data=None):#tracts=None, counties=None, small_counties=None):
+	def __init__(self, lar_schema=None, ts_schema=None, year=2018, crosswalk_data=None):#tracts=None, counties=None, small_counties=None):
 		#lar and TS field names (load from schema names?)
 		
 		#Loading yaml for geographic data.
@@ -22,12 +22,12 @@ class rules_engine(object):
 			geographic = yaml.safe_load(f)
 
 		self.year = year
-		self.tracts = list(cbsa_data.tractFips)#tracts #instantiate valid Census tracts
-		self.counties = list(cbsa_data.countyFips) #instantiate valid Census counties
-		self.small_counties = cbsa_data[cbsa_data.smallCounty=="1"]#small_counties #instantiate list of small counties
+		self.tracts = list(crosswalk_data.tractFips)#tracts #instantiate valid Census tracts
+		self.counties = list(crosswalk_data.countyFips) #instantiate valid Census counties
+		self.small_counties = crosswalk_data[crosswalk_data.smallCounty=="1"]#small_counties #instantiate list of small counties
 		self.lar_field_names = list(lar_schema.field)
 		self.ts_field_names = list(ts_schema.field)
-		self.cbsa_data = cbsa_data
+		self.crosswalk_data = crosswalk_data
 		#self.ts_df, self.lar_df= self.split_ts_row(data_file=data_file)
 		#if data_row:
 			#self.lar_df = data_row
@@ -2814,7 +2814,7 @@ class rules_engine(object):
 		self.lar_df['fail_flag'] = "" #set flag to filter lar_df by fail rows
 		#iterate over lar to match state code with list of counties inside the state
 		for index, row in self.lar_df.iterrows():
-			county_list = list(self.cbsa_data.countyFips[self.cbsa_data.stateCode==row["state"]])
+			county_list = list(self.crosswalk_data.countyFips[self.crosswalk_data.stateCode==row["state"]])
 			if row["county"] not in county_list:
 				self.lar_df.at[index,'fail_flag'] = "1"
 		fail_df = self.lar_df[(self.lar_df.fail_flag=="1")]
