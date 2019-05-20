728abeb76e980651c35d8c435d15b3bdfed64f69
Michael Furey <Michael.Furey@cfpb.gov>
2019-05-20 13:57:51 -0700

    changes to function call

--- python/run_custom_files.py
+++ python/run_custom_files.py
@@ -9,4 +9,6 @@ new_file = FileGenerator()
 new_file.create_custom_file(
 	yaml_filepath='custom_files/ffiec_logic/configurations/sex_categorization.yaml',
 	filepath='custom_files/ffiec_logic/output/',
- 	filename='sex_categorization_test.txt')
+ 	filename='sex_categorization_test.txt', 
+ 	filepath_answers='custom_files/ffiec_logic/answers/', 
+ 	filename_answers='sex_categorization_answers.txt')
