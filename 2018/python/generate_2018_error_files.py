d245fd3fc93d7d5278c968d96295058df8bf4558
MichaelFurey17 <43118127+MichaelFurey17@users.noreply.github.com>
2019-05-28 14:38:48 -0700

    adding description to 2018 error file

--- 2018/python/generate_2018_error_files.py
+++ 2018/python/generate_2018_error_files.py
@@ -1,4 +1,4 @@
-#This script will generate a file that will fail a specific edit (though others may also fail)
+#This script will generate a file that will fail a specific syntax, validity, or quality edit (though others may also fail)
 #This script relies on the presence of a clean data file
 
 from file_generator import FileGenerator 
